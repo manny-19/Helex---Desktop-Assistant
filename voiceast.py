e83350c03d92a9696fc3df67a89cc271f15390d7
Manvi <manvi019arora@gmail.com>
2021-06-12 14:14:08 +0530

    commiting changes

--- voiceast.py
+++ voiceast.py
@@ -1,13 +1,13 @@
-import pyttsx3
-import datetime
-
+import pyttsx3           # -m venv PATH OF FOLDER\venv  
+import datetime                  # use this to create virtual env if error in importing libraries
+import speech_recognition as sr
 engine  = pyttsx3.init('sapi5') # sapi5 is microsoft speech API , to use inbuilt voices
 voices = engine.getProperty('voices')
 #print(voices)         
 engine.setProperty('voice', voices[1].id)    #[1] == female voice, [0] == male voice
 
 def speak(audio):      #speak function inputs string
-    engine.say(audio)
+    engine.say(audio)  
     engine.runAndWait()
     
 def wishme():
@@ -22,6 +22,28 @@ def wishme():
     else:
         speak("Good Evening!")    
     
-    speak("Hi, I am Helex. PLease tell me how can I help you")
+    speak("Hey, I am Helex. PLease tell me how can I help you")
+    
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+    
 if __name__ == '__main__':
     wishme()
+    takeCommand()
